c87c7a6c4757b49d278203dd5cf650911928e9ac
Madasamy Mariappan <Madasamy.Mariappan@GlobalMantrai.com>
2019-10-16 23:13:12 +0530

    First commit With UI and API

---
+++ my-first-service/services/math.service.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+	name: "math",
+
+	actions: {
+
+
+		add(ctx) {
+			return Number(ctx.params.a) + Number(ctx.params.b);
+		},
+		sub(ctx) {
+			return Number(ctx.params.a) - Number(ctx.params.b);
+		}
+	},
+};
